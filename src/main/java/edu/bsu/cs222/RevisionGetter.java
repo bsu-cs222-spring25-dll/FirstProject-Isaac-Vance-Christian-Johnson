6413cfb056fd0fb6ff3844b941e4ac031f58b994
Isaac Vance <ivance5373@gmail.com>
2025-02-06 13:00:15 -0500

    Most recent code

--- src/main/java/edu/bsu/cs222/RevisionGetter.java
+++ src/main/java/edu/bsu/cs222/RevisionGetter.java
@@ -1,10 +1,17 @@
 package edu.bsu.cs222;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
-public class RevisionGetter() {
+public class RevisionGetter {
     public String revisionParse(String jsonData){
-        JSONObject object = new JSONObject(json);
-        JSONObject revisions = object.getJSONObject("revisions");
-        return revisions;
+        JSONArray revisionArray = new JSONArray();
+
+        return revisionArray.toString();
+
+    }
+    public String ArrayMaker(JSONObject jsonData){
+        return  (jsonData.getJSONObject("$..revisions").toString());
+
+
     }
 }
